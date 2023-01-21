12a34c6e7da210e7989bea367e04655b51fdbb95
Rajitkumar <krajit909@gmail.com>
2023-01-21 22:22:30 +0530

    bug fixed

--- videoPlayer/script.js
+++ videoPlayer/script.js
@@ -124,13 +124,7 @@ function skipAhead(event) {
 
 // updateVolume updates the video's volume
 // and disables the muted state if active
-function updateVolume() {
-  if (video.muted) {
-    video.muted = false;
-  }
 
-  video.volume = volume.value;
-}
 
 // updateVolumeIcon updates the volume icon so that it correctly reflects
 // the volume of the video
